dc736df22298a91182988865bdfcfda1c8243edb
Kaliszby <sumyung17qwq@gmail.com>
2021-12-20 11:07:32 +0700

    awesome

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import router from "./router"
+import store from "./store"
+import "./index.css";
+createApp(App).use(router).use(store).mount('#app')
